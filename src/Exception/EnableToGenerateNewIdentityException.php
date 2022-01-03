402cef2460b8f1d6c95b51d98dcf857caa18e5a2
Julien Mercier-Rojas <julien@jeckel-lab.fr>
2022-01-03 16:22:47 +0100

    Fix exception dep

--- src/Exception/EnableToGenerateNewIdentityException.php
+++ src/Exception/EnableToGenerateNewIdentityException.php
@@ -9,13 +9,14 @@ declare(strict_types=1);
 
 namespace JeckelLab\IdentityContract\Exception;
 
-use JeckelLab\Contract\Domain\Exception\LogicException;
 use JeckelLab\Contract\Domain\Identity\Exception\IdentityException;
+use LogicException;
 
 /**
  * Class EnableToGenerateNewIdentityException
  * @package JeckelLab\IdentityContract\Exception
  * @psalm-immutable
+ * @psalm-suppress MutableDependency
  */
 class EnableToGenerateNewIdentityException extends LogicException implements IdentityException
 {
